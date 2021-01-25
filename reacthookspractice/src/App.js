dbd2372686ed1e504a0985ba9a096618945bffc6
Nick Whaley <nicholasrwhaley@gmail.com>
2021-01-24 18:05:47 -0700

    add choice component

--- reacthookspractice/src/App.js
+++ reacthookspractice/src/App.js
@@ -3,26 +3,37 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Dropdown from 'react-bootstrap/Dropdown'
 
+
+function Menu() {
+  let[choice, setChoice] = useState('-')
+  return(
+    <div>
+      <p>You've selected the following: {choice}</p><br/>
+    <Dropdown>
+    <Dropdown.Toggle variant="success" id="dropdown-basic">
+      Dropdown Button
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu>
+      <Dropdown.Item onClick={() => setChoice(choice = 'Choice1')}>Choice1</Dropdown.Item><br/>
+      <Dropdown.Item onClick={() => setChoice(choice = 'Choice2')}>Choice2</Dropdown.Item><br/>
+      <Dropdown.Item onClick={() => setChoice(choice = 'Choice3')}>Choice3</Dropdown.Item><br/>
+    </Dropdown.Menu>
+  </Dropdown>
+  </div>
+  )
+}
+
 function App() {
   const[count, setCount] = useState(0);
-  let[choice, setChoice] = useState('-')
+
 
   return (
     <div className="App">
       <p>You clicked {count} times</p>
      <button onClick={() => setCount(count+1)}>Add Count</button>
-     <p>You've selected the following: {choice}</p>
-     <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
-    Dropdown Button
-  </Dropdown.Toggle>
 
-  <Dropdown.Menu>
-    <Dropdown.Item onClick={() => setChoice(choice = 'Choice1')}>Choice1</Dropdown.Item><br/>
-    <Dropdown.Item onClick={() => setChoice(choice = 'Choice2')}>Choice2</Dropdown.Item><br/>
-    <Dropdown.Item onClick={() => setChoice(choice = 'Choice3')}>Choice3</Dropdown.Item><br/>
-  </Dropdown.Menu>
-</Dropdown>
+    <Menu />
     </div>
   );
 }
